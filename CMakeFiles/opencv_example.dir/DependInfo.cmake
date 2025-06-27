d9a154c83ffc8b6662bac40ff5c273ddb0469d77
Volodymyr Leha <volodymyr.leha@gmail.com>
2025-06-27 21:40:54 +0300

    all files

---
+++ CMakeFiles/opencv_example.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/app/src/main.cpp" "CMakeFiles/opencv_example.dir/src/main.cpp.o" "gcc" "CMakeFiles/opencv_example.dir/src/main.cpp.o.d"
+  "/app/src/nano.cpp" "CMakeFiles/opencv_example.dir/src/nano.cpp.o" "gcc" "CMakeFiles/opencv_example.dir/src/nano.cpp.o.d"
+  "/app/src/tracker.cpp" "CMakeFiles/opencv_example.dir/src/tracker.cpp.o" "gcc" "CMakeFiles/opencv_example.dir/src/tracker.cpp.o.d"
+  "/app/src/tracker_runner.cpp" "CMakeFiles/opencv_example.dir/src/tracker_runner.cpp.o" "gcc" "CMakeFiles/opencv_example.dir/src/tracker_runner.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/opencv_example.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_example.dir/src/main.cpp.o"
+  "CMakeFiles/opencv_example.dir/src/main.cpp.o.d"
+  "CMakeFiles/opencv_example.dir/src/nano.cpp.o"
+  "CMakeFiles/opencv_example.dir/src/nano.cpp.o.d"
+  "CMakeFiles/opencv_example.dir/src/tracker.cpp.o"
+  "CMakeFiles/opencv_example.dir/src/tracker.cpp.o.d"
+  "CMakeFiles/opencv_example.dir/src/tracker_runner.cpp.o"
+  "CMakeFiles/opencv_example.dir/src/tracker_runner.cpp.o.d"
+  "opencv_example"
+  "opencv_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
